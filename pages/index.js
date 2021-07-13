f70d77db2985b54845c31a19a0e464e49972592c
Matteo Sacchi <matteoseik@hotmail.it>
2021-07-13 17:06:27 +0200

    mapbox

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { createClient } from "contentful"
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl';
+import { useState, useCallback } from 'react';
+import MapGL, {FlyToInterpolator} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2FjY2hpIiwiYSI6ImNrazFrdG5hNzBzM2oycW1manJvbDl6ZGoifQ.FOB7LTrZU3E4nF270xUyxA';
+
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoibWF0dGVvc2FjY2hpIiwiYSI6ImNrazFrdG5hNzBzM2oycW1manJvbDl6ZGoifQ.FOB7LTrZU3E4nF270xUyxA";
 
 
   export async function getStaticProps(){
-
     const client = createClient({
       space: process.env.CONTENTFUL_ID,
       accessToken:process.env.CONTENTFUL_TOKEN,
@@ -31,22 +32,23 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGVvc2FjY2hpIiwiYSI6ImNrazFrdG5hNzBzM2oyc
 
 export default function Home({ projects, about }) {
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-
-  if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/matteosacchi/ckr0ipix41y8p17mxlnaqerk7',
-      center: [lng, lat],
-      zoom: zoom
-    });
+  const [viewport, setViewport] = useState({
+  latitude: 37.7751,
+  longitude: -122.4193,
+  zoom: 11,
+  bearing: 0,
+  pitch: 0
   });
+
+  const onSelectProject = useCallback(({longitude, latitude}) => {
+    setViewport({
+      longitude,
+      latitude,
+      zoom: 11,
+      transitionInterpolator: new FlyToInterpolator({speed: 1.2}),
+      transitionDuration: 'auto'
+    });
+  }, []);
 
   return (
     <div className="map_container">
@@ -56,7 +58,17 @@ export default function Home({ projects, about }) {
       {about.map((item) => (
         <div key={item.sys.id}>{item.fields.title}</div>
       ))}
-      <div ref={mapContainer} className="map-container" />
+
+      <MapGL
+            {...viewport}
+            width="100%"
+            height="100%"
+            mapStyle="mapbox://styles/matteosacchi/ckr0ipix41y8p17mxlnaqerk7"
+            onViewportChange={setViewport}
+            dragRotate={false}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+
     </div>
   )
 }
