7d769202c8728082bae0166936a8d9d8be0685d0
Matteo Sacchi <matteoseik@hotmail.it>
2021-07-13 15:33:00 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-export default function Recipes() {
+import { createClient } from "contentful"
+
+  export async function getStaticProps(){
+
+    const client = createClient({
+      space: "eboqqdlmb7kk",
+      accessToken:"RCUA7VvunO68Ee5poATlJWqQ_5FDM690t9il_Wq8mSc",
+    })
+
+  const res = await client.getEntries({
+    content_type: 'projects'
+  })
+
+
+  return {
+    props: {
+      projects: res.items
+    }
+  }
+}
+
+
+export default function Home({ projects }) {
+console.log(projects)
   return (
     <div className="recipe-list">
-      Recipe List
+      {projects.map((item) => (
+        <div key={item.sys.id}>{item.fields.title}</div>
+      ))}
     </div>
   )
 }
